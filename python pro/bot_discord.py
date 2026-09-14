2c822a18b02c202e21971f8ee5e6177fe15b92e8
Miguel Gioseffi <mgioseffileal@icloud.com>
2026-09-14 20:21:23 -0300

    Obscure bot token in bot_discord.py

---
+++ python pro/bot_discord.py
@@ -0,0 +1,35 @@
+import discord
+from bot_logic import gen_pass
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+# Ativar a permissão para ler o conteúdo das mensagens
+intents.message_content = True
+# Criar um bot e passar as permissões
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Fizemos login como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+client.run("MTU0...")
